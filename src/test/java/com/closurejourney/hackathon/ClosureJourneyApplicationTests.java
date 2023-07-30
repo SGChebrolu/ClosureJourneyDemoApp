bbdb9a2ae2ed82baf5f37d93248a572bc75e90d5
SGChebrolu <sandeepguptha.ch@gmail.com>
2023-07-30 20:50:05 +0530

    Init working version

--- src/test/java/com/closurejourney/hackathon/ClosureJourneyApplicationTests.java
+++ src/test/java/com/closurejourney/hackathon/ClosureJourneyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.closurejourney.hackathon.swiggyclone;
+package com.closurejourney.hackathon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
